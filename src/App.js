94472475701d9778caa4dc4605c370b45a10653f
Graham <herdmanctg@outlook.com>
2019-10-13 15:33:13 +1100

    implemented react router and city in url query

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+// External Libraries
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 // My Components
 import WeatherWidget from "./components/WeatherWidget";
 
@@ -11,13 +14,16 @@ import Col from "react-bootstrap/Col";
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <Col md={{ span: 10, offset: 1 }} style={styles.appContainer}>
-            <WeatherWidget />
-          </Col>
-        </Row>
-      </Container>
+      <Router>
+        <Container>
+          <Row>
+            <Col md={{ span: 10, offset: 1 }} style={styles.appContainer}>
+              <Route path="/" component={WeatherWidget} />
+              {/* <WeatherWidget /> */}
+            </Col>
+          </Row>
+        </Container>
+      </Router>
     );
   }
 }
--- src/components/WeatherWidget.js
+++ src/components/WeatherWidget.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+// External Libraries
+import queryString from "query-string";
+
 // My Components
 import Header from "./Header/Header";
 import InformationBar from "./InformationBar/InformationBar";
@@ -13,33 +16,42 @@ import { getTodaysWeather, getFutureWeather } from "../API/weatherAPI";
 class WeatherWidget extends Component {
   constructor() {
     super();
-    this.state = { today: {}, forecast: {}, currentDate: new Date() };
+    this.state = {
+      city: "Sydney",
+      today: {},
+      forecast: {},
+      currentDate: new Date()
+    };
     this.fetchWeatherForecast = this.fetchWeatherForecast.bind(this);
   }
 
   componentDidMount() {
-    this.fetchWeatherForecast();
+    const queryCity = queryString.parse(this.props.history.location.search)
+      .city;
+    const city = queryCity ? queryCity : this.state.city;
+    this.props.history.replace("");
+    this.props.history.push(`?city=${city}`);
+    this.fetchWeatherForecast(city);
   }
 
-  async fetchWeatherForecast(city = "sydney") {
-    /**
-     * Need two API calls 1) for current weather 2) for next 4 days weather
-     * why?
-     * because a request to the forecast api endpoint can return an empty array for todays weather if oyu are within
-     * 3 hours of the end of the day.
-     */
-
+  async fetchWeatherForecast(city = this.state.city) {
     // Update current date object
     this.setState({ currentDate: new Date() });
 
     // get weather for today
-    getTodaysWeather(city).then(
-      ({ todaysTemperature, todaysWeatherConditionID }) => {
+    getTodaysWeather(city)
+      .then(({ todaysTemperature, todaysWeatherConditionID }) => {
         this.setState({
           today: new WeatherObject(todaysTemperature, todaysWeatherConditionID)
         });
-      }
-    );
+      })
+      .catch(error => {
+        alert(
+          `Error: Invalid city entered in the url \nPlease enter a valid city, \nredirecting to Sydney weather`
+        );
+        this.props.history.push(`?city=Sydney`);
+        this.fetchWeatherForecast("Sydney");
+      });
 
     // get weather forecast
     getFutureWeather(city).then(forecast => {
